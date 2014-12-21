5125bdaa0733af2c66b6737f2af2451283ac6194
dkl <daniel.c.klauer@web.de>
2014-12-21 22:46:37 +0100

    winapi: ws2def

--- winapi.mk
+++ winapi.mk
@@ -216,6 +216,7 @@ WINAPI_BASE += winspool
 WINAPI_BASE += winsvc
 WINAPI_BASE += winuser
 WINAPI_BASE += winver
+WINAPI_BASE += ws2def
 WINAPI_BASE += ws2spi
 WINAPI_BASE += ws2tcpip
 #WINAPI_BASE += wsahelp
