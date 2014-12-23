d6cc7f49bd9340f5148238dfcf8a018639a15957
dkl <daniel.c.klauer@web.de>
2014-12-23 20:33:02 +0100

    winapi: mprapi

--- winapi.mk
+++ winapi.mk
@@ -115,6 +115,7 @@ WINAPI_BASE += mapi
 WINAPI_BASE += mcx
 WINAPI_BASE += mgmtapi
 WINAPI_BASE += mmsystem
+WINAPI_BASE += mprapi
 WINAPI_BASE += msacm
 WINAPI_BASE += mshtmhst
 WINAPI_BASE += mshtml
