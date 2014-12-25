e6c82a17c4501b3232aa09c4ce65a69cce1de8f0
dkl <daniel.c.klauer@web.de>
2014-12-25 16:28:25 +0100

    winapi: propkeydef

--- winapi.mk
+++ winapi.mk
@@ -157,6 +157,7 @@ WINAPI_BASE += oledlg
 WINAPI_BASE += oleidl
 WINAPI_BASE += powrprof
 WINAPI_BASE += propidl
+WINAPI_BASE += propkeydef
 WINAPI_BASE += propsys
 WINAPI_BASE += prsht
 WINAPI_BASE += psapi
