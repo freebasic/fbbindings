2989137f57a84f86e701d6af9602e8bd0a7373ad
dkl <daniel.c.klauer@web.de>
2015-04-18 14:51:58 +0200

    check-bi.sh: Exclude non-generated .bi's from the check for fbfrog titles

--- check-bi.sh
+++ check-bi.sh
@@ -4,15 +4,32 @@ set -e
 function check_bi() {
 	bi="$1"
 
-	line1="`head -1 $bi`"
-	line3="`head -3 $bi | tail -1`"
+	case "$bi" in
 
-	case "${line1}${line3}" in
-	"'' FreeBASIC binding for "*"'' based on the C header files:")
+	# Exclude the custom (non-generated) files from the check for fbfrog-style titles
+	"inc/curses.bi"|\
+	"inc/gdk/gdk.bi"|\
+	"inc/GL/gl.bi"|\
+	"inc/GL/glext.bi"|\
+	"inc/GL/glu.bi"|\
+	"inc/gtkgl/gdkgl.bi"|\
+	"inc/gtkgl/gtkgl.bi"|\
+	"inc/gtk/gtk.bi"|\
+	"inc/png.bi"|\
+	"inc/win/ole-common.bi")
 		;;
+
 	*)
-		echo $bi
-		;;
+		line1="`head -1 $bi`"
+		line3="`head -3 $bi | tail -1`"
+
+		case "${line1}${line3}" in
+		"'' FreeBASIC binding for "*"'' based on the C header files:")
+			;;
+		*)
+			echo $bi
+			;;
+		esac
 	esac
 
 	if grep -xq "#pragma once" "$bi"; then
