7f5e6b1d47eca6f8886ca3f6e1521001bfd9a5b9
dkl <daniel.c.klauer@web.de>
2015-04-24 14:28:58 +0200

    get.sh: Add 4th parameter for creating toplevel dir

--- get.sh
+++ get.sh
@@ -4,6 +4,7 @@ set -e
 dirname="extracted/$1"
 tarball="tarballs/$2"
 url="$3"
+createdir="$4"
 
 mkdir -p tarballs/ extracted/
 
@@ -12,12 +13,16 @@ if [ ! -f "$tarball" ]; then
 fi
 
 if [ ! -d "$dirname" ]; then
-	case "$tarball" in
-	*.zip)
-		unzip -q -d extracted/ "$tarball"
-		;;
-	*)
-		tar xf "$tarball" -C extracted/
-		;;
-	esac
+	if [ "$createdir" = "createdir" ]; then
+		mkdir "$dirname"
+		case "$tarball" in
+		*.zip) unzip -q -d "$dirname/" "$tarball";;
+		*)     tar xf "$tarball" -C "$dirname/";;
+		esac
+	else
+		case "$tarball" in
+		*.zip) unzip -q -d extracted/ "$tarball";;
+		*)     tar xf "$tarball" -C extracted/;;
+		esac
+	fi
 fi
