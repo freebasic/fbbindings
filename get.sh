0285ff70d4894a340e7eedc9080c2aefb0b27b1c
Stéphane Aulery <lkppo@free.fr>
2018-08-07 23:24:50 +0200

    - Add a resume option for  aborted or failed download of sources - Don't extract if sources download failed

--- get.sh
+++ get.sh
@@ -8,21 +8,22 @@ createdir="$4"
 
 mkdir -p tarballs/ extracted/
 
-if [ ! -f "$tarball" ]; then
-	wget "$url" -O "$tarball"
-fi
+# If the download failed previously, try to resume it before 
+wget -c "$url" -O "$tarball"
 
-if [ ! -d "$dirname" ]; then
-	if [ "$createdir" = "createdir" ]; then
-		mkdir "$dirname"
-		case "$tarball" in
-		*.zip) unzip -q -d "$dirname/" "$tarball";;
-		*)     tar xf "$tarball" -C "$dirname/";;
-		esac
-	else
-		case "$tarball" in
-		*.zip) unzip -q -d extracted/ "$tarball";;
-		*)     tar xf "$tarball" -C extracted/;;
-		esac
+if [ $? -eq 0 ]; then
+	if [ ! -d "$dirname" ]; then
+		if [ "$createdir" = "createdir" ]; then
+			mkdir "$dirname"
+			case "$tarball" in
+			*.zip) unzip -q -d "$dirname/" "$tarball";;
+			*)     tar xf "$tarball" -C "$dirname/";;
+			esac
+		else
+			case "$tarball" in
+			*.zip) unzip -q -d extracted/ "$tarball";;
+			*)     tar xf "$tarball" -C extracted/;;
+			esac
+		fi
 	fi
 fi
