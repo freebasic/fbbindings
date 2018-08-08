211beefaf3ddcf0042fd55615e3a3e5145ca5402
Stéphane Aulery <lkppo@free.fr>
2018-08-08 22:09:50 +0200

    If the download failed previously, try to resume it before Increase the max number of redirection to 30 because the mirror autoselect function of SourceForge can test all mirrors before finding the closest (08/08/2018 : there are 25 mirrors)

--- getxorg.sh
+++ getxorg.sh
@@ -7,8 +7,12 @@ url="$3"
 
 mkdir -p tarballs/xorg extracted/xorg
 
-# If the download failed previously, try to resume it before 
-wget --no-verbose -c "$url" -O "$tarball"
+# If the download failed previously, try to resume it before
+# Increase the max number of redirection to 30 because
+# the mirror autoselect function of SourceForge can test
+# all mirrors before finding the closest
+# (08/08/2018 : there are 25 mirrors)
+wget --no-verbose -c --max-redirect=30 "$url" -O "$tarball"
 
 if [ $? -eq 0 ]; then
 	if [ ! -d "$dirname" ]; then
