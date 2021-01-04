73542bc4bc86cb63cac10f612ceef4346a07c0ab
Ralph Versteegen <teeemcee@gmail.com>
2021-01-04 22:12:09 +1300

    get.sh: Don't do anything if already extracted.
    This restores the previous behaviour before 0285ff70d489, but resuming
    interrupted downloads still works.

--- get.sh
+++ get.sh
@@ -6,28 +6,30 @@ tarball="tarballs/$2"
 url="$3"
 createdir="$4"
 
+if [ -d "$dirname" ]; then
+	exit
+fi
+
 mkdir -p tarballs/ extracted/
 
-# If the download failed previously, try to resume it before
+# If the download failed previously, try to resume it.
+# (WARNING: if the file is modified and increases in length,
+# this will create garbage!)
 # Increase the max number of redirection to 30 because
 # the mirror autoselect function of SourceForge can test
 # all mirrors before finding the closest
 # (08/08/2018 : there are 25 mirrors)
-wget -c --max-redirect=30 "$url" -O "$tarball"
+wget -c --max-redirect=30 "$url" -O "$tarball" || exit 1
 
-if [ $? -eq 0 ]; then
-	if [ ! -d "$dirname" ]; then
-		if [ "$createdir" = "createdir" ]; then
-			mkdir "$dirname"
-			case "$tarball" in
-			*.zip) unzip -q -d "$dirname/" "$tarball";;
-			*)     tar xf "$tarball" -C "$dirname/";;
-			esac
-		else
-			case "$tarball" in
-			*.zip) unzip -q -d extracted/ "$tarball";;
-			*)     tar xf "$tarball" -C extracted/;;
-			esac
-		fi
-	fi
+if [ "$createdir" = "createdir" ]; then
+	mkdir "$dirname"
+	case "$tarball" in
+	*.zip) unzip -q -d "$dirname/" "$tarball";;
+	*)     tar xf "$tarball" -C "$dirname/";;
+	esac
+else
+	case "$tarball" in
+	*.zip) unzip -q -d extracted/ "$tarball";;
+	*)     tar xf "$tarball" -C extracted/;;
+	esac
 fi
