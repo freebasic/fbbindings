e72bda68ad94e56d57cbff9819050fb9df1ebfe4
Ralph Versteegen <teeemcee@gmail.com>
2021-01-06 21:59:44 +1300

    get.sh: support downloading single .h files

--- get.sh
+++ get.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #
-# If not already extracted, download a .tar.gz or .zip to tarballs/ and extract it to extracted/.
-# These names can include a subdirectory prefix.
+#  ./get.sh <output> <tarball> <url> [createdir]
 #
+# If not already downloaded+extracted, download $url (a .tar.gz or .zip or
+# single .h) to tarballs/$tarball and extract/copy it to extracted/$output/.
+# $output and $tarball can include a subdirectory prefix.
+
 set -ex
 
 output="extracted/$1"
@@ -31,12 +34,14 @@ wget -c --max-redirect=30 "$url" -O "$tarball" || exit 1
 if [ "$createdir" = "createdir" ]; then
 	mkdir "$output"
 	case "$tarball" in
-	*.zip) unzip -q -d "$output/" "$tarball";;
-	*)     tar xf "$tarball" -C "$output/";;
+	*.zip)  unzip -q -d "$output/" "$tarball";;
+	*.tar*) tar xf "$tarball" -C "$output/";;
+	*)      cp -a "$tarball" "$output/";;
 	esac
 else
 	case "$tarball" in
-	*.zip) unzip -q -d "$outputdir/" "$tarball";;
-	*)     tar xf "$tarball" -C "$outputdir/";;
+	*.zip)  unzip -q -d "$outputdir/" "$tarball";;
+	*.tar*) tar xf "$tarball" -C "$outputdir/";;
+	*)      cp -a "$tarball" "$output/";;
 	esac
 fi
